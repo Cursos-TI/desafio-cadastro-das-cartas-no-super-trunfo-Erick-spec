27964274cdc070e44e0cc2bf1447fbef3cb1a2c0
Erick-spec <erickmateuss07@gmail.com>
2025-03-19 15:52:01 +0000

    Carta 1 lida

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@ int main() {
     int populacao1;
     float area1;
     float pib1;
-    int pontosTuristicos;
+    int pontosTuristicos1;
 
     printf("Informe os dados da carta 1: \n");
     printf("Estado (A-H): ");
@@ -16,7 +16,21 @@ int main() {
     scanf("%s", codigo1);
     printf("Nome da cidade:");
     scanf("%49[^\n]", nomeCidade1);
+    printf("População: ");
+    scanf("%d", &populacao1);
+    printf("Área (em Km²): ");
+    scanf("%f", &area1);
+    printf("Pib (em bilhões de reias): ");
+    scanf("%f", &pib1);
+    printf("Número de pontos Turísticos: ");
+    scanf("%d", &pontosTuristicos1);
+
+
+    printf("\nCarta 1:\n");
     
+
+
+
    
     
 
