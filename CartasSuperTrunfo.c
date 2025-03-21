c02ae5612fda4f232a6286d60054e53cdf556833
Erick-spec <erickmateuss07@gmail.com>
2025-03-21 15:27:18 +0000

    Conclusão CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -38,27 +38,40 @@ int main() {
     char estado2;
     char codigo2[4];
     char nomeCidade2[20];
-    int pupolacao2;
+    int populacao2;
     float area2;
     float pib2;
     int pontosTuristicos2;
 
-    
-    
+
+    printf("Informe os dados da Carta 1: \n");
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado1);
+    printf("Código da carta: ");
+    scanf("%s", codigo2);
+    printf("Nome da cidade: ");
+    scanf(" %49[^\n]", nomeCidade2);
+    printf("Populção: ");
+    scanf("%d", &populacao2);
+    printf("Área (em Km²): ");
+    scanf("%f", &area2);
+    printf("PIB (em bilhões de reais): ");
+    scanf("%f", &pib2);
+    printf("Número de Pontos Turísticos");
+    scanf("%d", &pontosTuristicos2);
 
 
-
-   
-    
-
-
-
-
-
-
-
-
+    printf("\n Carta 2: \n");
+    printf("Estado: %c \n", estado2);
+    printf("Código: %s \n", codigo2);
+    printf("Nome da Cidade: %s \n", nomeCidade2);
+    printf("População: %d \n", populacao2);
+    printf("Área: %.2f km² \n", area2);
+    printf("PIB: %2.f bilhões de reais \n", pib2);
+    printf("Número de Pontos Turísticos: %d \n", pontosTuristicos2);
 
 
     return 0;
+
+
 }
